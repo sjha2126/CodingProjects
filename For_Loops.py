b50e6e9297c372ee2334ce07820dd5b46a931d21
Srijan Jha <srijan2126@gmail.com>
2026-08-05 13:16:30 -0500

    Delete For_Loops.py

--- For_Loops.py
+++
@@ -1,8 +0,0 @@
-from turtle import Turtle
-tracy=Turtle()
-for lineColor in ["red","green","blue","yellow"]:
-    tracy.pensize(5)
-    tracy.color(lineColor)
-    tracy.forward(100)
-    tracy.left(90)
-
