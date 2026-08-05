f43919a7fcbe3edf3ac7514a784f9e940f569f46
Srijan Jha <srijan2126@gmail.com>
2026-08-05 13:17:24 -0500

    Delete Tilted_Square.py

--- Tilted_Square.py
+++
@@ -1,8 +0,0 @@
-from turtle import Turtle
-anwesha=Turtle()
-anwesha.speed(0)
-for x in range(30):
-    for i in range(4):
-        anwesha.forward(50)
-        anwesha.left(90)
-    anwesha.left(10)
