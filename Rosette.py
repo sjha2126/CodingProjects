e2c83bcbdfb8612b461606ab3af675cab825e67d
Srijan Jha <srijan2126@gmail.com>
2026-08-05 13:17:13 -0500

    Delete Rosette.py

--- Rosette.py
+++
@@ -1,9 +0,0 @@
-import turtle
-tracy=turtle.Pen()
-tracy.speed(0)
-n=int(turtle.numinput("How many circles would you want in a circle? ",6))
-for i in range(n):
-    tracy.circle(100)
-    tracy.left(360/n)
-
-   
